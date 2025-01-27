d724503d6f01ac7c33660b58d1962a67a8f0d78d
Kartik Tambi <kartiktambi07@gmail.com>
2025-01-27 18:25:43 +0530

    Update qrcode.php
    second try

--- qrcode.php
+++ qrcode.php
@@ -1,9 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-$id = isset($_GET['id']) ? htmlspecialchars($_GET['id']) : '';
+$id = isset($_GET['id']) ? $_GET['id'] : '';
 if($id) {
-    $url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" . urlencode($id);
+    $url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" . $id;
     echo $url;
 } else {
     echo "ID parameter is missing.";
 }
 ?>
+
+</body>
+</html>
